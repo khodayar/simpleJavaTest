746ee0912db046e5c5f97b77dcf03c48622f9762
Kody <khodayar.jeirroodi@sap.com>
2021-05-28 08:20:35 -0400

    add two sections

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,12 +4,25 @@ public class DemoApplication {
 
     public static void main(String[] args) {
 
-        DataHolder myDataHolder = new DataHolder();
+        DataHolder myDataHolder;
 
-        new Greeting("Hello");
-        new Greeting("World!");
 
+        // first part
+        // expected output: Hello World!
+
+        myDataHolder = new DataHolder();
+        myDataHolder.addData("hello");
+        myDataHolder.addData("world!");
         System.out.println(myDataHolder);
+
+
+
+        //second part: comment out the lines 11..14
+
+        new Greeting("hello");
+        new Greeting("world!");
+        System.out.println(myDataHolder);
+
         // expected output: Hello World!
 
     }
